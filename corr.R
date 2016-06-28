22298bb11bdab6d62afa01d68b0d6ced3d808590
Leonid Khaylov <leonid.khaylov@operationalrisksolutions.com>
2016-06-28 12:18:49 +0200

    Code refactoring

--- corr.R
+++ corr.R
@@ -17,29 +17,29 @@ corr <- function(directory, threshold = 0) {
   output <- numeric(length = 0)
   
   # 1. Validate input parameters
-  if (class(directory) == 'character' & (class(threshold) == 'integer' | class(threshold) == 'numeric')) {
+  if (!(class(directory) == 'character' & (class(threshold) == 'integer' | class(threshold) == 'numeric')))
+    return(output)
     
-    # 1.1. Calculate frequency of complete data rows for each given monitor ID with the help of table() function
-    completeDataRowsFreq <- complete(directory)
-    
-    # 1.2. Get the monitor IDs after filtering
-    filteredMonitorIds <- completeDataRowsFreq["id"][completeDataRowsFreq["nobs"] >= threshold]
-    
-    # 1.3. Get the data from the CSV files. Return in case the data could not be read.
-    polluteData <- getPolluteData(directory, as.integer(filteredMonitorIds))
-    if ( is.null(polluteData) )
-      return(output)
-    
-    # 1.4. Filter out data rows where sulfate or nitrate value is missing.
-    filteredDataRows <- polluteData[ !is.na(polluteData[,"sulfate"]) & !is.na(polluteData[,"nitrate"]), ]
-    
-    # 1.5. Iterate through monitor IDs
-    for (id in filteredMonitorIds) {
-      # 1.5.1. Calculate correlation between sulfate and nitrate values and add them to the output vector
-      output <- c(output, cor(filteredDataRows[filteredDataRows["ID"]==id,"sulfate"], filteredDataRows[filteredDataRows["ID"]==id,"nitrate"]))
-    }
+  # 2. Calculate frequency of complete data rows for each given monitor ID with the help of complete() function
+  completeDataRowsFreq <- complete(directory)
+  
+  # 3. Get the monitor IDs after filtering
+  filteredMonitorIds <- completeDataRowsFreq["id"][completeDataRowsFreq["nobs"] >= threshold]
+  
+  # 4. Get the data from the CSV files. Return in case the data could not be read.
+  polluteData <- getPolluteData(directory, as.integer(filteredMonitorIds))
+  if ( is.null(polluteData) )
+    return(output)
+  
+  # 5. Filter out data rows where sulfate or nitrate value is missing.
+  filteredDataRows <- polluteData[ !is.na(polluteData[,"sulfate"]) & !is.na(polluteData[,"nitrate"]), ]
+  
+  # 6. Iterate through monitor IDs
+  for (id in filteredMonitorIds) {
+    # 6.1. Calculate correlation between sulfate and nitrate values and add them to the output vector
+    output <- c(output, cor(filteredDataRows[filteredDataRows["ID"]==id,"sulfate"], filteredDataRows[filteredDataRows["ID"]==id,"nitrate"]))
   }
   
-  # 2. Return the numeric vector
+  # 7. Return the numeric vector
   output
 }
